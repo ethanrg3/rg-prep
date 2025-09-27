f594e00442d5d72bdc0ee5265500973e434fb217
ethanrg03 <erodgersgates@gmail.com>
2025-09-27 17:54:15 -0400

    create venv and gitignore

--- raw_score_converter.py
+++ raw_score_converter.py
@@ -1,4 +1,7 @@
 # This is a CL script that takes raw ACT scores and outputs scaled and super ACT scores
+from collections import defaultdict
 
 class RawScore:
-    self.
+    answer_key = defaultdict(str)
+
+
